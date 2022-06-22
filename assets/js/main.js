2e10878e270b012cf87462a22b79756375b97cf1
hendrik.loewe <hendrikloewe3@gmail.com>
2022-06-22 17:32:27 +0200

    Preview image in large on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const IMAGES_MAX_COUNT = [33, 22, 25];
 const categories = ['general', 'detail', 'portrait'];
 const imageContainer = document.getElementById('image-list');
+const imagePreview = document.getElementById('image-preview');
 
 let selectedCategory;
 
@@ -30,6 +31,13 @@ function loadImagesIntoContainer(basePath, maxCount) {
   for (let i = 0; i < maxCount; i++) {
     const newImage = document.createElement('img');
     newImage.src = `${basePath}/${i}.jpg`;
+
+    newImage.onclick = (event) => {
+      imagePreview.appendChild(newImage.cloneNode(false));
+      imagePreview.classList.add('active');
+      document.body.classList.add('scroll-disabled');
+    }
+
     imageContainer.appendChild(newImage);
   }
 }
@@ -43,6 +51,12 @@ document.querySelectorAll('.select').forEach((item, index) => {
     console.log('foo');
     setSelectedCategory(index);
   })
+})
+
+imagePreview.addEventListener('click', () => {
+  imagePreview.classList.remove('active');
+  imagePreview.innerHTML = '';
+  document.body.classList.remove('scroll-disabled');
 })
 
 setSelectedCategory(0);
